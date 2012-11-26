7ecd47fe3d51f05609318b19bf1e336e7a30d61b
Ondřej Piálek <ondrej@pialek.eu>
2012-11-26 22:36:47 +0200

    reword the seen/unseen converter text to match context

---
+++ wp7/SeriesTracker/Converters/BooleanToSeenConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+
+namespace SeriesTracker
+{
+    public class BooleanToSeenConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            var val = value as bool?;
+
+            if (val == true)
+                return "Mark as not seen";
+            else
+                return "Mark as seen";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
